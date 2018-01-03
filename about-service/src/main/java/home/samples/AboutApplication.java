c24c213e5998f04cd5ce0b7151dd036d731540e2
akaczmarek <adam.kaczmarek@onwelo.com>
2018-01-03 10:03:35 +0100

    Adding zuul service - setting/reading correlationId feature.

--- about-service/src/main/java/home/samples/AboutApplication.java
+++ about-service/src/main/java/home/samples/AboutApplication.java
@@ -2,16 +2,22 @@ package home.samples;
 
 
 import home.samples.context.UserContextHolder;
+import home.samples.context.UserContextInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Collections;
+import java.util.List;
 
 
 @SpringBootApplication
@@ -33,20 +39,21 @@ public class AboutApplication {
     }
 
     // HOW TO SET CORRELATION ID
-//    @Bean
-//    public RestTemplate getRestTemplate(){
-//        RestTemplate template = new RestTemplate();
-//        List interceptors = template.getInterceptors();
-//        if (interceptors==null){
-//            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-//        }
-//        else{
-//            interceptors.add(new UserContextInterceptor());
-//            template.setInterceptors(interceptors);
-//        }
-//
-//        return template;
-//    }
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        RestTemplate template = new RestTemplate();
+        List interceptors = template.getInterceptors();
+        if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AboutApplication.class, args);
